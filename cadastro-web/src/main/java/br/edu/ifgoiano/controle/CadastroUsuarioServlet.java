0d0997f73d401b8825aac6b9850ad3fc3b20d393
Hudson Romualdo <hudson.paula@gmail.com>
2023-04-10 09:14:49 -0300

    Melhorias no código

--- cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServlet.java
+++ cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServlet.java
@@ -1,6 +1,8 @@
 package br.edu.ifgoiano.controle;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,9 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.edu.ifgoiano.entidade.Usuario;
+
 @WebServlet("/cadastrarUsuario")
 public class CadastroUsuarioServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 7869758393435911873L;
+	
+	//Simular o banco de dados
+	private List<Usuario> lstDeUsuario;
+	
+	@Override
+	public void init() throws ServletException {
+		this.lstDeUsuario = new ArrayList<Usuario>();
+	}
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String senha1 = req.getParameter("senha1");
@@ -18,12 +32,31 @@ public class CadastroUsuarioServlet extends HttpServlet {
 		
 		//Verificar se as senhas são iguais
 		if(senha1.equals(senha2)) {
+			Usuario usu = new Usuario();
+			usu.setNome(req.getParameter("nome"));
+			usu.setEmail(req.getParameter("email"));
+			usu.setSenha(senha1);
+			
+			lstDeUsuario.add(usu);
+			
 			//redirecionar o usuário para a página de login
 			resp.sendRedirect("index.html");
 		}else {
 			//redirecionar o usuário para a mesma página de cadastro do usuário.
 			req.getRequestDispatcher("cadastroUsuario.html").forward(req, resp);
 		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		for (Usuario usuario : lstDeUsuario) {
+			System.out.println(usuario.getNome().concat(" - ").concat(usuario.getEmail()));
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		this.lstDeUsuario.clear();
 	}
 }
 
